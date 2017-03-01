d7f1877ff68d978ad9eb3f5e0ea9f512ce718aad
Павел <freestalker1987@gmail.com>
2017-03-01 17:18:13 +0300

    Added some editingg task features

--- todolist/src/app/components/task-component/tasks/task/changeButton/changeButton.component.ts
+++ todolist/src/app/components/task-component/tasks/task/changeButton/changeButton.component.ts
@@ -7,57 +7,86 @@ import {TaskManagerService} from "../../../../servicies/TaskManagerService";
 import {Task} from "../../../../../accessoryClasses/task/Task";
 import * as moment from 'moment';
 import Moment = moment.Moment;
-import {IMyOptions,IMyDateModel} from "mydatepicker";
+import {IMyOptions, IMyDateModel} from "mydatepicker";
 
 @Component({
-    selector: 'changeDropDownMenu',
-    templateUrl: 'ChangeDropDownMenu.html',
-    styleUrls:['ChangeDropDownMenu.css']
+  selector: 'changeDropDownMenu',
+  templateUrl: 'changeButton.component.html',
+  styleUrls: ['changeButton.component.css']
 })
 export class ChangeDropDownMenu implements OnInit {
   @Input()
-  public task:Task;
+  public task: Task;
 
-    private myDatePickerOptions: IMyOptions = {
+  private postponeDatePickerOptions: IMyOptions = {
     dateFormat: 'dd.mm.yyyy',
-    width:'50%',
-    todayBtnTxt: 'Today'
+    width: '100%',
+    todayBtnTxt: 'Today',
+    disableUntil: {year: moment().year(), month: moment().month(), day: moment().date()}
   };
 
-  private date={
-    date:
-    {
-      year:moment().year(),
-      month:moment().month(),
-      day:moment().date()
+  private postponeDate = {
+    date: {
+      year: moment().year(),
+      month: moment().month(),
+      day: moment().date()
     }
   };
 
   constructor(private taskManagerService: TaskManagerService) {
   }
 
-  ngOnInit() { }
-
-  postponeOneDay(){
-      let newDate:Moment=this.task.date.add(1,'days');
-      this.taskManagerService.postponeTask(this.task,newDate);
+  ngOnInit() {
   }
 
-  postponeOneWeek(){
-      let newDate:Moment=this.task.date.add(7,'days');
-      this.taskManagerService.postponeTask(this.task,newDate);
+  setPriority($event) {
+    let priorityID: string = event.target['id'];
+    let priority: number;
+    switch (priorityID) {
+      case('priority1'): {
+        priority = 1;
+        break;
+      }
+      case('priority2'): {
+        priority = 2;
+        break;
+      }
+      case('priority3'): {
+        priority = 3;
+        break;
+      }
+      case('priority4'): {
+        priority = 4;
+        break;
+      }
+
+
+    }
   }
 
-  onDateChanged(event:IMyDateModel){
-      this.postponeTask();
+  postponeOneDay() {
+    let newDate: Moment = moment().hour(0).minutes(0).second(0).milliseconds(0);
+    newDate.add(1, 'days');
+    this.taskManagerService.postponeTask(this.task, newDate);
   }
 
-  private postponeTask(){//TODO:refactor this sheet
-      let date:Moment=moment()
-      .date(this.date.date.day)
-      .month(this.date.date.month)
-      .year(this.date.date.year);
-      this.taskManagerService.postponeTask(this.task,date);
+  postponeOneWeek() {
+    let newDate: Moment = moment().hour(0).minutes(0).second(0).milliseconds(0);
+    newDate.add(1, 'week');
+    this.taskManagerService.postponeTask(this.task, newDate);
+  }
+
+  onPostponeCalendarDateChanged(event: IMyDateModel) {
+    this.postponeTask();
+  }
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  private postponeTask() {//TODO:refactor this sheet
+    let date: Moment = moment([this.postponeDate.date.year, this.postponeDate.date.month, this.postponeDate.date.day])
+    this.taskManagerService.postponeTask(this.task, date);
   }
 
 }
