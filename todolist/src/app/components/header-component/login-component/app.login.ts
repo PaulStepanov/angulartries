644d4612a5213c47aac76bec7df4d58d45655f87
zarabon <freestalker1987@gmail.com>
2017-04-16 14:37:33 +0300

    fix prev commit

--- todolist/src/app/components/header-component/login-component/app.login.ts
+++ todolist/src/app/components/header-component/login-component/app.login.ts
@@ -1,3 +1,23 @@
-/**
- * Created by pili on 4/16/17.
- */
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from "../../../servicies/LoginService";
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-login-button',
+  templateUrl: 'app.login.html',
+  styleUrls: ['app.login.css']
+})
+export class LoginComponent implements OnInit {
+  private userName:string;
+  private isLogged:boolean = false;
+
+  constructor(private loginService:LoginService) {
+  }
+
+  ngOnInit() {
+  }
+
+  logout(){
+    this.loginService.logout();
+  }
+}
